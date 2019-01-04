706a321032730d738af72207181bb4a8a274aca4
Sebastien Bovo <sebastienbovo@outlook.com>
2019-01-04 17:43:25 +0100

    Scripts modifications

--- Lab/Exercise3/01-Start/Contoso.Dashboard/js/sb-pwa.js
+++ Lab/Exercise3/01-Start/Contoso.Dashboard/js/sb-pwa.js
@@ -1,10 +1,10 @@
 if (navigator.serviceWorker.controller) {
-    console.log('[PWA Builder] active service worker found, no need to register');
+    console.log('[PWA Builder] Active service worker found, no need to register');
 } else {
   //Register the ServiceWorker
   navigator.serviceWorker.register('sw.js', {
     scope: './'
   }).then(function(reg) {
-    console.log('Service worker has been registered for scope:'+ reg.scope);    
+    console.log('[PWA Builder] Service worker has been registered for scope: ' + reg.scope);    
   });
 }
